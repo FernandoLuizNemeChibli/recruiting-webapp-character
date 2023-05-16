9d52b6a0af84e67ff64ab4c99bef65dd91317de7
FernandoLuizNemeChibli <fernandoluiz.1991@gmail.com>
2023-05-16 19:10:15 -0300

    feat ClassPanel sends informations to ClassElement

--- src/components/ClassPanel/index.js
+++ src/components/ClassPanel/index.js
@@ -3,13 +3,15 @@ import { CLASS_LIST } from "../../consts"
 import React from "react"
 import ClassElement from "../ClassElement"
 
-export default function ClassPanel({}) {
+export default function ClassPanel({ charAttributes }) {
   return (
-    <Stack direction="column">
+    <Stack direction="column" spacing={2}>
       {Object.keys(CLASS_LIST).map((className) => (
         <ClassElement
           key={`attributeElement${className}`}
           className={className}
+          attributesRequirements={CLASS_LIST[className]}
+          charAttributes={charAttributes}
         />
       ))}
     </Stack>
