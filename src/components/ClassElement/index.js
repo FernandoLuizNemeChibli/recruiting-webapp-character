2e1dd28e9c56c89f75e739d56581c9af1f86aaad
FernandoLuizNemeChibli <fernandoluiz.1991@gmail.com>
2023-05-16 18:05:16 -0300

    feat ClassElement WIP

---
+++ src/components/ClassElement/index.js
@@ -0,0 +1,10 @@
+import { Button } from "@mui/material"
+import React from "react"
+
+export default function ClassElement({ className }) {
+  return (
+    <>
+      <Button>{className}</Button>
+    </>
+  )
+}
