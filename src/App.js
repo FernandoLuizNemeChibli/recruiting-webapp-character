2a3938325cb2be2b47ae0a1f6ae5aa9875c264ed
FernandoLuizNemeChibli <fernandoluiz.1991@gmail.com>
2023-05-16 17:39:53 -0300

    feat AttributePanel

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import { useState } from 'react';
-import './App.css';
-import { ATTRIBUTE_LIST, CLASS_LIST, SKILL_LIST } from './consts.js';
-
+import React, { useState } from "react"
+import "./App.css"
+import { ATTRIBUTE_LIST, CLASS_LIST, SKILL_LIST } from "./consts.js"
+import { Stack } from "@mui/material"
+import AttributePanel from "./components/AttributePanel"
+import Player from "./classes/player"
 
 function App() {
-  const [num, setNum] = useState(0);
+  const player = new Player("player name placeholder")
   return (
     <div className="App">
       <header className="App-header">
         <h1>React Coding Exercise</h1>
       </header>
       <section className="App-section">
-        <div>
-          Value:
-          {num}
-          <button>+</button>
-          <button>-</button>
-        </div>
+        <Stack direction="row">
+          <AttributePanel charAttributes={player.attributes} />
+        </Stack>
       </section>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/AttributePanel/index.js
@@ -0,0 +1,37 @@
+import { Stack } from "@mui/material"
+import React, { useEffect, useState } from "react"
+import { ATTRIBUTE_LIST } from "../../consts"
+import AttributeElement from "../AttributeElement"
+
+export default function AttributePanel({ charAttributes }) {
+  const [attributes, setAttributes] = useState(charAttributes)
+
+  console.log(attributes)
+
+  const createAttributeSetter = (attributeName) => {
+    //
+    const attributeSetter = (newValue) => {
+      console.log(newValue, attributeName)
+      setAttributes((previousAttributes) => ({
+        ...previousAttributes,
+        [attributeName]: {
+          ...previousAttributes[attributeName],
+          points: newValue,
+        },
+      }))
+    }
+    return attributeSetter
+  }
+  return (
+    <Stack direction="column">
+      {ATTRIBUTE_LIST.map((attributeName) => (
+        <AttributeElement
+          key={`attributeElement${attributeName}`}
+          attributeName={attributeName}
+          attributeValue={attributes[attributeName].points}
+          setAttributeValue={createAttributeSetter(attributeName)}
+        />
+      ))}
+    </Stack>
+  )
+}
