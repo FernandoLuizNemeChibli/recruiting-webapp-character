8d3a749a73df0b2f78483fe70d362d6b88006296
FernandoLuizNemeChibli <fernandoluiz.1991@gmail.com>
2023-05-16 19:09:31 -0300

    feat ClassElement useEffect checks attributes values

--- src/components/ClassElement/index.js
+++ src/components/ClassElement/index.js
@@ -1,10 +1,36 @@
 import { Button } from "@mui/material"
-import React from "react"
+import React, { useEffect, useState } from "react"
+import { ATTRIBUTE_LIST } from "../../consts"
 
-export default function ClassElement({ className }) {
+export default function ClassElement({
+  className,
+  attributesRequirements,
+  charAttributes,
+}) {
+  const [classDisabled, setClassDisabled] = useState(true)
+
+  useEffect(() => {
+    let newDisabledValue = false
+    ATTRIBUTE_LIST.forEach((attributeName) => {
+      console.log("checking ", attributeName)
+      if (
+        charAttributes[attributeName].points <
+        attributesRequirements[attributeName]
+      ) {
+        console.log(
+          charAttributes[attributeName].points,
+          attributesRequirements[attributeName]
+        )
+        newDisabledValue = true
+      }
+    })
+    console.log(newDisabledValue)
+    setClassDisabled(newDisabledValue)
+  }, [charAttributes])
+
   return (
-    <>
-      <Button>{className}</Button>
-    </>
+    <Button variant="contained" disabled={classDisabled}>
+      {className}
+    </Button>
   )
 }
