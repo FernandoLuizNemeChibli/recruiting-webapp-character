f3920ec67356923c534a228b73ca7f3843ae974d
FernandoLuizNemeChibli <fernandoluiz.1991@gmail.com>
2023-05-16 17:39:35 -0300

    feat AttributeElement

---
+++ src/components/AttributeElement/index.js
@@ -0,0 +1,36 @@
+import { Button, Typography } from "@mui/material"
+import { Add, Remove } from "@mui/icons-material"
+
+import React from "react"
+
+export default function AttributeElement({
+  attributeName,
+  attributeValue,
+  setAttributeValue,
+}) {
+  const increaseAttribute = () => {
+    setAttributeValue(attributeValue + 1)
+  }
+  const decreaseAttribute = () => {
+    setAttributeValue(attributeValue - 1)
+  }
+  return (
+    <div>
+      <Typography>
+        {attributeName}: {attributeValue}{" "}
+      </Typography>
+      <Button
+        variant="outlined"
+        size="large"
+        startIcon={<Add />}
+        onClick={increaseAttribute}
+      />
+      <Button
+        variant="outlined"
+        size="large"
+        startIcon={<Remove />}
+        onClick={decreaseAttribute}
+      />
+    </div>
+  )
+}
