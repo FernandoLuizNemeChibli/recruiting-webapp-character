982f8c86c0ea29367b8bc7be25fc1f3d67268059
FernandoLuizNemeChibli <fernandoluiz.1991@gmail.com>
2023-05-17 16:26:40 -0300

    feat ClassPanel control modal states

--- src/components/ClassPanel/index.js
+++ src/components/ClassPanel/index.js
@@ -1,9 +1,17 @@
 import { Stack } from "@mui/material"
 import { CLASS_LIST } from "../../consts"
-import React from "react"
+import React, { useState } from "react"
 import ClassElement from "../ClassElement"
+import ClassInfoDialog from "../ClassInfoDialog"
 
 export default function ClassPanel({ charAttributes }) {
+  const [classInfoName, setClassInfoName] = useState("")
+  const [classInfoModalOpen, setClassInfoModalOpen] = useState(false)
+
+  const callModal = (className) => {
+    setClassInfoName(className)
+    setClassInfoModalOpen(true)
+  }
   return (
     <Stack direction="column" spacing={2}>
       {Object.keys(CLASS_LIST).map((className) => (
@@ -12,8 +20,14 @@ export default function ClassPanel({ charAttributes }) {
           className={className}
           attributesRequirements={CLASS_LIST[className]}
           charAttributes={charAttributes}
+          callModal={callModal}
         />
       ))}
+      <ClassInfoDialog
+        open={classInfoModalOpen}
+        onClose={() => setClassInfoModalOpen(false)}
+        className={classInfoName}
+      />
     </Stack>
   )
 }
